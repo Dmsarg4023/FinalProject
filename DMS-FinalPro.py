57974469d2a5baa403ce1da350607eefb414405c
Dmsarg4023 <Dmsarg4023@users.noreply.github.com>
2014-07-31 15:20:49 -0400

    Update DMS-FinalPro.py
    Added las files to work the program

--- DMS-FinalPro.py
+++ DMS-FinalPro.py
@@ -1,4 +1,3 @@
-## Taking LAS Files & Conveting Them Into TIFF Files.
 import datetime
 import os
 import glob
@@ -6,7 +5,7 @@ import arcpy
 from arcpy import env
 env.workspace = ()
 outgdb = ()
-
+"""Your LAS files are now converted into TIFF files."""
 las = glob.glob('*.las')
 print(las)
 for las in las:
@@ -16,7 +15,16 @@ for las in las:
     output_name = time_string.split('.')[0].replace(":", "_")
 
     las_path = r'exports\{}_{}'.format(name, output_name)
-    las = arcpy.mapping.MapDocument(las)
+    las =  = "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2525014450.las"
+    "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2525014500.las"
+    "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2525014550.las"
+    "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2530014450.las"
+    "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2530014500.las"
+    "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2530014550.las"
+    "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2535014450.las"
+    "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2535014500.las"
+    "E:\\SandyCreekProject\\LASPolygons\\LAS Files\\GAW2535014550.las",
+    arcpy.mapping.MapDocument(las)
     print(os.path.join(os.getcwd(), lasd_path))
 
     arcpy.CreateLasDataset_management(las, os.path.join(os.getcwd(), lasd_path))
@@ -24,7 +32,8 @@ for las in las:
     arcpy.mapping.LasDatasetToRaster_conversion(las, os.path.join(os.getcwd(), lasd_path))
     
     lasdesc = arcpy.Describe(lasd)
-    arcpy.CopyFeatures_management(lasd, os.path.join(outgdb, lasdesc.basename))
-    
+    arcpy.CopyFeatures_management(lasd, os.path.join(outgdb, lasdesc.basename))    
 
     arcpy.mapping.RasterToOtherFormat_conversion(lasd, os.path.join(os.getcwd(), tif_path))
+
+    
