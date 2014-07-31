c3c846f2fd3e7ca335a77993b6a2b70326bf5016
Dmsarg4023 <Dmsarg4023@users.noreply.github.com>
2014-07-31 13:07:09 -0400

    Update DMS-FinalPro.py
    Project modification, changed description and added more to the code.

--- DMS-FinalPro.py
+++ DMS-FinalPro.py
@@ -1,9 +1,30 @@
-## This program will take multiple .las files and 
-#   convert them to one .dem then to one Geo .tif file
-
-import arcpy
+## Taking LAS Files & Conveting Them Into TIFF Files.
+import datetime
 import os
+import glob
+import arcpy
 from arcpy import env
-arcpy.CheckOutExtension('3D')
-env.workspace = 'C:/data'
-arcpy.LasDatasetToRaster_3d
+env.workspace = ()
+outgdb = ()
+
+las = glob.glob('*.las')
+print(las)
+for las in las:
+    name = las.split('.')[0]
+    
+    time_string = datetime.datetime.now().isoformat()
+    output_name = time_string.split('.')[0].replace(":", "_")
+
+    las_path = r'exports\{}_{}'.format(name, output_name)
+    las = arcpy.mapping.MapDocument(las)
+    print(os.path.join(os.getcwd(), lasd_path))
+
+    arcpy.CreateLasDataset_management(las, os.path.join(os.getcwd(), lasd_path))
+
+    arcpy.mapping.LasDatasetToRaster_conversion(las, os.path.join(os.getcwd(), lasd_path))
+    
+    lasdesc = arcpy.Describe(lasd)
+    arcpy.CopyFeatures_management(lasd, os.path.join(outgdb, lasdesc.basename))
+    
+
+    arcpy.mapping.RasterToOtherFormat_conversion(lasd, os.path.join(os.getcwd(), tif_path))
